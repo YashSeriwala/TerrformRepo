9d2c8511fab6ccc3075dcb74e628a93f54853dcc
YashSeriwala <140882606+YashSeriwala@users.noreply.github.com>
2025-02-05 19:10:58 +0530

    Delete AWS-Terrform-module directory

--- AWS-Terrform-module/module/private_ec2/private_ec2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "ec2"{
-    instance_type = var.instance_type[count.index]
-    subnet_id = var.subnet_id
-    ami = var.ami
-}
--- AWS-Terrform-module/module/vpc/data.tf
+++
@@ -1,7 +0,0 @@
-# data "aws_nat_gateway" "default" {
-#   subnet_id = aws_subnet.public_subnet.id
-# }
-
-# data "aws_eip" "default" {
-#   allocation_id = "eipalloc-02191f2e7896851e6"
-# }
